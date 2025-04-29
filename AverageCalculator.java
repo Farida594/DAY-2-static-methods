bc4b66558a85fc6a787f06d07f724178c7691fa1
Farida594 <faridaapril45@gmail.com>
2025-04-29 20:32:05 +0530

    Add files via upload

---
+++ AverageCalculator.java
@@ -0,0 +1,8 @@
+public class AverageCalculator{
+public static void averageofthreeNumbers(double a, double b, double c ){
+System.out.println("Average:"+(a+b+c)/3);
+}
+public static void main(String[] args){
+averageofthreeNumbers(10.0,20.0,30.0);
+}
+}
---
+++ SimpleInterestCalculator.java
@@ -0,0 +1,8 @@
+public class SimpleInterestCalculator{
+public static void calculateSimpleInterest(double principle, double rate, double time ){
+System.out.println("Simple Interest:"+(1000.0*5.0* 2.0)/100);
+}
+public static void main(String[] args){
+calculateSimpleInterest(10.0,20.0,30.0);
+}
+}
